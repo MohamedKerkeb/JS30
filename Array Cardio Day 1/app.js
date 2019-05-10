172ad8546b4b3a33bd456f2bf97c6dc2bd3dce34
MohamedWild <kerkeb.moha@gmail.com>
2019-05-10 12:51:07 +0200

    5 days of JS30

---
+++ Array Cardio Day 1/app.js
@@ -0,0 +1,114 @@
+const inventors = [
+  { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
+  { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
+  { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
+  { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
+  { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
+  { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
+  { first: "Max", last: "Planck", year: 1858, passed: 1947 }
+];
+
+const people = [
+  "Beck, Glenn",
+  "Becker, Carl",
+  "Beckett, Samuel",
+  "Beddoes, Mick",
+  "Beecher , Henry",
+  "Beethoven, Ludwig",
+  "Begin, Menachem",
+  "Belloc, Hilaire",
+  "Bellow, Saul",
+  "Benchley, Robert",
+  "Benenson, Peter",
+  "Ben-Gurion, David",
+  "Benjamin, Walter",
+  "Benn, Tony",
+  "Bennington, Chester",
+  "Benson, Leana",
+  "Bent, Silas",
+  "Bernhard, Sandra",
+  "Berra, yogi",
+  "Berry, Halle",
+  "Berry, Wendell",
+  "Bethea, Erin",
+  "Bevan, Aneurin",
+  "Bevel, Ken",
+  "Biden, Josheph",
+  "Bierce, Ambrose",
+  "Biko, Steve",
+  "Billings, josh",
+  "Biondo, Frank",
+  "Birrell, Augustine",
+  "Black, Elk",
+  "Blair, Robert",
+  "Blair, Tony",
+  "Blake, William"
+];
+
+// Filter
+const filter = inventors.filter(born => born.year >= 1500 && born.year < 1600);
+
+console.table(filter);
+
+// Map
+const map = inventors.map(el => `${el.first} ${el.last}`);
+console.table(map);
+
+// sort
+const sort = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
+console.table(sort);
+
+// reduce
+const reduce = inventors.reduce((total, inventors) => {
+  return total + (inventors.passed - inventors.year);
+}, 0);
+
+console.log(reduce);
+
+// sort 2
+const sort2 = inventors.sort((a, b) => {
+  const lastGuy = a.passed - a.year;
+  const nextGuy = b.passed - b.year;
+  return lastGuy > nextGuy ? -1 : 1;
+});
+console.table(sort2);
+
+//exercice sort
+// Sort the people alphabetically by last name
+
+const alpha = people.sort((a, b) => {
+  const [aLast, aFirst] = a.split(", ");
+  const [bLast, bFirst] = b.split(", ");
+  return aLast > bLast ? 1 : -1;
+});
+
+console.log(alpha);
+
+// Recude Exercice
+/// sum up the instances of each of these
+const data = [
+  "car",
+  "car",
+  "truck",
+  "truck",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "car",
+  "truck"
+];
+
+const transportation = data.reduce((obj, item) => {
+  if (!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+}, {});
+
+console.log(transportation);
